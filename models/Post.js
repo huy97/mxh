3f41f7d783cd1437d0ead2ff5299d763fd399286
Huy Le <huylt@dtsgroup.com.vn>
2020-12-05 00:08:38 +0700

    Update

--- models/Post.js
+++ models/Post.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-    title: {type: String, default: "", required: true},
+    title: {type: String, default: ""},
     content: {type: String, default: "", required: true, trim: true},
     userId: {type: mongoose.Types.ObjectId, required: true},
     comment: {type: Number, default: 0},
